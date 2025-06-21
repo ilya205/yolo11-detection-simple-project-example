0937603d425e4f7cf23c89c2d2a54912c32dfa71
Ilya Bychkov <ilbych12@mail.ru>
2025-06-22 00:32:26 +0300

    setup.py corrected

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file."""
+    with open(filename) as f:
+        requirements = []
+        for line in f:
+            # Skip comments, -e, and -r lines
+            if line.startswith('#') or line.startswith('-e') or line.startswith('-r'):
+                continue
+            # Remove whitespace and newlines
+            requirement = line.strip()
+            if requirement:  # Add non-empty lines
+                requirements.append(requirement)
+        return requirements
+
 
 setup(
-    name="YOLO11 example",          # Package name (e.g., "myproject")
+    name="yolo11-detection-simple-project-example",          # Package name (e.g., "myproject")
     version="0.0.1",                   # Version (semantic versioning)
     packages=find_packages(),          # Automatically discover packages
-    install_requires=requirements,     # Dependencies from requirements.txt
+    install_requires=parse_requirements('requirements.txt'),     # Dependencies from requirements.txt
     python_requires=">=3.11",           # Minimum Python version
     author="Ilya Bychkov",                # Author name
     author_email='',

c84a48c072cc89c1bda1dd0137a5a634b99f6855
Ilya Bychkov <ilbych12@mail.ru>
2025-06-21 21:46:20 +0300

    First release with all functions

---
+++ main.py
@@ -0,0 +1,177 @@
+import os.path
+import sys
+import logging
+from PySide6 import QtWidgets, QtGui, QtCore
+import model
+import ui_main
+
+
+class QTextEditLogger(logging.Handler):
+
+    def __init__(self, text_edit):
+        super().__init__()
+        formatter = logging.Formatter(
+            fmt='[%(levelname)s] %(asctime)s - %(module)s.%(lineno)d - %(message)s\n',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        self.setFormatter(formatter)
+        self.text_edit = text_edit
+
+    def emit(self, record):
+        self.text_edit.moveCursor(QtGui.QTextCursor.End)
+        self.text_edit.textCursor().insertText(self.format(record))
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = ui_main.Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowTitle("YOLO11 example")
+        self.create_menu_bar()
+        self.model_thread = model.ModelThread(self)  # used to launch training in separate thread
+        self.set_signals()
+        self.configure_logger()
+
+    def set_signals(self):
+        self.ui.trainPushButton.clicked.connect(self.start_training)
+        self.ui.abortPushButton.clicked.connect(self.abort_training)
+        self.ui.loadWeightsPushButton.clicked.connect(self.select_weights)
+        self.ui.useWeightsCheckBox.stateChanged.connect(self.use_weights_checkbox_state_changed)
+        self.ui.selectFileForAnalysisPushButton.clicked.connect(self.select_file_to_analyse)
+        self.ui.analysePushButton.clicked.connect(self.analyse_file)
+        self.model_thread.update_gui_signal.connect(self.update_training_info)
+
+    def analyse_file(self):
+
+        if self.set_confidence_threshold():
+            self.model_thread.analyse_image()
+
+    def set_confidence_threshold(self):
+        try:
+            conf = float(self.ui.confidenceThresholdLineEdit.text())
+            if 1.0 >= conf >= 0.0:
+                self.model_thread.set_prediction_confidence_threshold(conf)
+                return True
+            else:
+                logging.warning('Wrong confidence threshold. Value not set.')
+                return False
+        except Exception as ex:
+            logging.warning(str(ex))
+            return False
+
+    def plot_image_on_label(self, image_path: str):
+        label = self.ui.analysisLabel
+        pixmap = QtGui.QPixmap(image_path)
+        scaled_pixmap = pixmap.scaled(
+            label.width(),
+            label.height(),
+            QtCore.Qt.KeepAspectRatio,
+            QtCore.Qt.SmoothTransformation
+        )
+        label.setPixmap(scaled_pixmap)
+
+    def update_training_info(self, trainer_dict):
+        """trainer_dict -
+        epoch: int (current epoch number)
+        epochs: int (total epochs number)
+        epoch_progress: int (progress in %)
+        metrics: str (metrics info)"""
+        if not bool(trainer_dict):
+            return
+        self.ui.epochNumberProgressLabel.setText(str(trainer_dict['epoch']) + '/' + str(trainer_dict['epochs']))
+        self.ui.currentEpochProgressBar.setValue(trainer_dict['epoch_progress'])
+        self.ui.metricsTextEdit.setText(trainer_dict['metrics'])
+
+    def start_training(self):
+        self.setup_training_settings()
+        self.model_thread.start()
+
+    def use_weights_checkbox_state_changed(self):
+        if self.ui.useWeightsCheckBox.isChecked():
+            self.model_thread.load_weights_to_model()
+        else:
+            self.model_thread.cancel_weight_load()
+
+    def setup_training_settings(self):
+        """Applies settings in trainingSettingsGroupBox"""
+        try:
+            epochs = int(self.ui.epochsNumberLineEdit.text())
+            self.model_thread.set_epochs_number(epochs)
+            batch = int(self.ui.batchSizeLineEdit.text())
+            self.model_thread.set_batch_number(batch)
+            imgsz = int(self.ui.imageSizeLineEdit.text())
+            self.model_thread.set_image_size(imgsz)
+            device = self.ui.deviceLineEdit.text()
+            self.model_thread.set_device_type(device)
+        except Exception as ex:
+            logging.warning('Error in training parameters\n' + str(ex))
+
+    def abort_training(self):
+        self.model_thread.abort_training()
+
+    def configure_logger(self):
+        logging.basicConfig(level=logging.INFO,
+                            format="%(asctime)s %(levelname)s %(message)s")
+        # add custom handler to log to textEdit
+        log = QTextEditLogger(self.ui.logTextEdit)
+        logging.getLogger().addHandler(log)
+
+    def create_menu_bar(self):
+        # Create menu bar
+        menu_bar = self.menuBar()
+
+        # File menu
+        file_menu = menu_bar.addMenu("&File")
+
+        # New action
+        new_action = QtGui.QAction("&Dataset", self)
+        new_action.triggered.connect(self.select_dataset)
+        new_action.setToolTip("Select .yaml file with dataset in YOLO11 format")
+        file_menu.addAction(new_action)
+
+    def select_file_to_analyse(self):
+        file_path, _ = QtWidgets.QFileDialog.getOpenFileName(
+            self,
+            "Select File",
+            "")
+        if os.path.exists(file_path):
+            self.model_thread.set_analysed_file_path(file_path)
+            # self.plot_image_on_label(file_path)
+        else:
+            logging.info('Wrong path for file')
+
+    def select_dataset(self):
+        """Selects file (.yaml) with dataset for ML"""
+        file_path, _ = QtWidgets.QFileDialog.getOpenFileName(
+            self,
+            "Select File",
+            "")
+        if os.path.exists(file_path):
+            self.model_thread.set_dataset_path(file_path)
+            logging.info('Dataset path updated')
+        else:
+            logging.info('Wrong path for dataset')
+
+    def select_weights(self):
+        """Selects weights file for future load to model"""
+        file_path, _ = QtWidgets.QFileDialog.getOpenFileName(
+            self,
+            "Select File",
+            "")
+        if os.path.exists(file_path):
+            self.model_thread.set_weights_path(file_path)
+            logging.info('Weights path updated')
+            if self.ui.useWeightsCheckBox.isChecked():
+                self.model_thread.load_weights_to_model()
+        else:
+            logging.info('Wrong path for weights')
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
---
+++ model.py
@@ -0,0 +1,123 @@
+import os.path
+import sys
+
+from PySide6 import QtCore
+from ultralytics import YOLO
+import cv2
+import logging
+
+logger = logging.getLogger()
+
+
+class ModelThread(QtCore.QThread):
+    update_gui_signal = QtCore.Signal(object)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.weights_path = ''
+        self._current_epoch_step = 0  # used to calculate epoch progress (incremented for every batch calc)
+        self.train_settings = {'data': "./dataset/data.yaml",
+                               'epochs': 1,
+                               'batch': 8,
+                               'imgsz': 320,
+                               'device': 'cpu'}
+        self.model = YOLO("yolo11n.yaml")
+        self._should_stop = False  # used to stop model training from gui
+        self._analysed_file_path = './cars examples/1.PNG'  # path to file to analyse with model
+        self._predict_confidence_threshold = 0.1  # confidence threshold used when prediction results plot
+
+    def abort_training(self):
+        self._should_stop = True
+        self.quit()
+
+    def cancel_weight_load(self):
+        """defines model without weights"""
+        self.model = YOLO("yolo11n.yaml")
+        logger.info('Weights discarded')
+
+    def load_weights_to_model(self):
+        try:
+            self.model.load(self.weights_path)
+            logger.info('Weights applied')
+        except Exception as ex:
+            logger.warning('Need to setup correct path for weights file\n' + str(ex))
+
+    def analyse_image(self):
+        """Analyses image self._analysed_file_path and plots result in window for confidence level
+        self._predict_confidence_level"""
+        img = cv2.imread(self._analysed_file_path)
+        results = self.model(img, conf=self._predict_confidence_threshold)
+        res = results[0]
+        res.show()
+
+    def run(self):
+        logger.info('Start training')
+        self._should_stop = False
+        try:
+            self.model.add_callback("on_train_batch_end", self._on_train_batch_end)
+            self.model.add_callback("on_train_epoch_start", self._on_epoch_begins)  # used to reset self._current_epoch_step
+            self.model.add_callback("on_train_end", self._on_train_end)  # used to indicate finish in log
+            self.model.train(
+                data=self.train_settings['data'],
+                epochs=self.train_settings['epochs'],
+                batch=self.train_settings['batch'],
+                imgsz=self.train_settings['imgsz'],
+                device=self.train_settings['device'],
+            )
+        except Exception as ex:
+            logger.error(str(ex))
+
+    def _on_train_batch_end(self, trainer):
+        """Called on every batch end to update gui in main thread"""
+        # abort if needed
+        if self._should_stop:
+            trainer.should_stop = True
+            trainer.validator = None
+            trainer.model = None
+            return
+        # iterate butch number
+        self._current_epoch_step += 1
+        # define text for metrics
+        metrics = ''
+        for index, val in enumerate(trainer.loss_items):
+            metrics += trainer.loss_names[index] + ': ' + str(val) + '\n'
+        trainer_dict = {'epoch': trainer.epoch + 1,
+                        'epochs': trainer.epochs,
+                        'epoch_progress': round(self._current_epoch_step / len(trainer.train_loader) * 100),
+                        'metrics': metrics}
+        self.update_gui_signal.emit(trainer_dict)
+
+    def _on_epoch_begins(self, batch):
+        self._current_epoch_step = 0
+
+    @staticmethod
+    def _on_train_end(trainer):
+        training_info_str = 'Training finished.\n'
+        training_info_str += 'Result directory:' + str(trainer.save_dir) + '\n'
+        for key in trainer.metrics:
+            training_info_str += key + ':' + str(trainer.metrics[key]) + '\n'
+        logger.info(training_info_str)
+
+    def set_dataset_path(self, dataset_path: str):
+        self.train_settings['data'] = str(dataset_path)
+
+    def set_weights_path(self, weights_path: str):
+        self.weights_path = str(weights_path)
+
+    def set_epochs_number(self, epochs: int):
+        self.train_settings['epochs'] = epochs
+
+    def set_batch_number(self, batch: int):
+        self.train_settings['batch'] = batch
+
+    def set_image_size(self, imgsz: int):
+        self.train_settings['imgsz'] = imgsz
+
+    def set_device_type(self, device_type: str):
+        self.train_settings['device'] = device_type
+
+    def set_analysed_file_path(self, filepath: str):
+        self._analysed_file_path = str(filepath)
+
+    def set_prediction_confidence_threshold(self, conf: float):
+        self._predict_confidence_threshold = conf
